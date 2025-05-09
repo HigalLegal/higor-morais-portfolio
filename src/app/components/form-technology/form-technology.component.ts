796c9437a320940c75e2cbd39600dbe459281bbc
Higor Morais de Lima0 <moraislimahigor@outlook.com>
2025-05-09 05:39:31 -0300

    Ajuste botão enviar do componente form-technology

--- src/app/components/form-technology/form-technology.component.ts
+++ src/app/components/form-technology/form-technology.component.ts
@@ -1,25 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-form-technology',
     standalone: true,
-    imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+    imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, CommonModule],
     templateUrl: './form-technology.component.html',
     styleUrls: [
         './form-technology.component.scss',
         './form-technology.component.responsive.scss',
     ],
 })
-export class FormTechnologyComponent {
+export class FormTechnologyComponent implements DoCheck {
     name: string = '';
     importanceLevel: number | null = null;
 
+    disableButton: boolean = true;
+
+    ngDoCheck(): void {
+        this.disableButton = !(this.name.length > 2 && !!this.importanceLevel);
+    }
+
     onSubmit() {
-        if (this.name && this.importanceLevel && this.importanceLevel >= 1) {
+        if (this.name && this.importanceLevel) {
             const request = {
                 name: this.name,
                 importanceLevel: this.importanceLevel,
