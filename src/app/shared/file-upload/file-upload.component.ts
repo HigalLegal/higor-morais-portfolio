387911c6ac65f0cff9e468c610d4d2bc553d034a
Higor Morais de Lima0 <moraislimahigor@outlook.com>
2025-05-12 06:42:03 -0300

    Implementando i18n no componente de upload de imagem + pequenos ajustes no estilo do mesmo

--- src/app/shared/file-upload/file-upload.component.ts
+++ src/app/shared/file-upload/file-upload.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { TranslateConfigService } from '../../services/translate-config-service/translate-config-service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-file-upload',
@@ -7,4 +9,23 @@ import { MatIconModule } from '@angular/material/icon';
     templateUrl: './file-upload.component.html',
     styleUrl: './file-upload.component.scss',
 })
-export class FileUploadComponent {}
+export class FileUploadComponent {
+    private readonly TRANSLATE_JSON: string = 'fileUpload';
+
+    message: string = '';
+
+    constructor(private translate: TranslateConfigService) {}
+
+    private recoverValue(key: string): Observable<string> {
+            return this.translate.retrieveKeyValueObservable(
+                `${this.TRANSLATE_JSON}.${key}`,
+            );
+        }
+
+    private insertI18N(): void {
+        this.recoverValue('escolher').subscribe({
+            next: escolher => { this.message = escolher; },
+            error: err => { console.error('Erro inesperado! ', err) }
+    });
+    }
+}
